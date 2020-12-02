e63028a8dbed4e18efcdb8dfadf3aa95d3d6676f
George McKinney <george@zukinimobile.com>
2020-12-01 17:19:30 -0800

    remove directory

--- module/linketservice/src/V1/Rest/Ls/LsCollection.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace linketservice\V1\Rest\Ls;
-
-use Laminas\Paginator\Paginator;
-
-class LsCollection extends Paginator
-{
-}
--- module/linketservice/src/V1/Rest/Ls/LsEntity.php
+++
@@ -1,6 +0,0 @@
-<?php
-namespace linketservice\V1\Rest\Ls;
-
-class LsEntity
-{
-}
